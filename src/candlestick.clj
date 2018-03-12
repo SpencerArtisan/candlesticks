8118008368b7abc11967b81882f3c5014b93ad47
Spencer <spencerartisan@gmail.com>
2018-03-12 22:52:41 +1000

    Cumulate trips

--- src/candlestick.clj
+++ src/candlestick.clj
@@ -1,5 +1,6 @@
 (ns candlestick
-  (:require [java-time :refer [local-date]]))
+  (:require [java-time :as time]
+            [clojure.java.io :as io]))
 
 
 (def itinerary "
@@ -11,12 +12,35 @@ Sicily     │        ─███──
   
 (defn add-trip
   [trips [what start end]]
-  (letfn [(->date [text] (local-date "d/M/yy" (str text "/18")))] 
+  (letfn [(->date [text] (time/local-date "d/M/yy" (str text "/18")))] 
     (conj trips {:what what :start (->date start) :end (->date end)})))
+
+(defn format-trip
+  [{:keys [what start end]}]
+  (letfn [(->str [date] (time/format "d MMM" date))]
+    (str (format "%-10s" what) (->str start) " - " (->str end))))
+
+(defn ->edn
+  [{:keys [what start end]}]
+  (letfn [(->str [date] (time/format "dd/MM/yyyy" date))]
+    [what (->str start) (->str end)])) 
+
+(defn ->trip
+  [[what start end]]
+  (letfn [(->date [text] (time/local-date "dd/MM/yyyy" text))]
+    {:what what :start (->date start) :end (->date end)}))
+
+(defn load-trips
+  []
+  (if (.exists (io/file "trips.edn"))
+    (map ->trip (read-string (slurp "trips.edn")))
+    []))
 
 (def actions
   {:draw #(println itinerary)
-   :add  (fn [args] (prn args) (spit "trips.txt" (add-trip [] args)))}) 
+   :add  (fn [args] (spit "trips.edn" (pr-str (map ->edn (add-trip (load-trips) args))))) 
+   :list (fn [args] (println (clojure.string/join "\n" (map format-trip (load-trips)))))})
+
 
 (defn -main [action & args]
   (let [func (get actions (keyword action))]
--- test/candlestick_test.clj
+++ test/candlestick_test.clj
@@ -4,8 +4,13 @@
     [expectations :refer [expect]]
     [java-time :refer [local-date]]))
 
-(expect [{:what "place" :start (local-date 2018 2 1) :end (local-date 2018 4 3)}]
+(def trip {:what "place" :start (local-date 2018 2 1) :end (local-date 2018 4 3)})
+
+(expect [trip]
         (c/add-trip [] ["place" "1/2" "3/4"]))
 
+(expect "place     1 Feb - 3 Apr"
+        (c/format-trip trip)) 
 
-
+(expect trip
+        (c/->trip (c/->edn trip)))
