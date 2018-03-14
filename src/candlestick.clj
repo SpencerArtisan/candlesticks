50e7d315b95a80480f0dc0a46bf969f3968961ad
Spencer <spencerartisan@gmail.com>
2018-03-14 21:45:29 +1000

    Use some threading macro

--- src/candlestick.clj
+++ src/candlestick.clj
@@ -4,19 +4,33 @@
             [chart :as chart]
             [trip :as trip]))
 
-(def actions
-  {:draw (fn [args](->> (trip/load-trips)
-                        (chart/chart 116 (now) (add (now) (->duration 9 :month)))))
-   :add  (fn [args] (-> (trip/load-trips)
-                        (trip/add-trip args)
-                        (trip/save-trips)))
-   :list (fn [args] (->> (trip/load-trips)
-                         (map trip/format-trip)
-                         (clojure.string/join "\n")))})
+(defn draw-chart
+  [trips args]
+  (->> trips
+       (chart/chart 116 (now) (add (now) (->duration 9 :month)))))
 
+(defn add-trip
+  [trips args]
+  (-> trips
+      (trip/add-trip args)
+      (trip/save-trips)))
+
+(defn list-trips
+  [trips args]
+  (->> trips
+       (map trip/format-trip)
+       (clojure.string/join "\n")))
+
+(def actions
+  {:draw draw-chart
+   :add  add-trip
+   :list list-trips})
 
 (defn -main [action & args]
-  (let [func (get actions (keyword action))
-        result (func args)]
-    (when result (println result))))
+  (let [trips (trip/load-trips)]
+    (some-> action
+            keyword
+            actions
+            (apply [trips args])
+            println)))
 
