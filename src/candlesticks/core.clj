192b32540789e60ebf65a1cd77eee55353f1a816
Spencer <spencerartisan@gmail.com>
2018-03-15 12:13:54 +1000

    Draw chart after some commands

--- src/candlesticks/core.clj
+++ src/candlesticks/core.clj
@@ -5,46 +5,46 @@
             [clojure.java.io :as io]))
 
 (defn draw-chart
-  [trips args]
-  (->> trips
-       (chart/chart 117 (now) (add (now) (->duration 9 :month)))))
+  [args]
+  (->> (trip/load-trips)
+       (chart/chart 117 (now) (add (now) (->duration 9 :month)))
+       println))
 
 (defn add-trip
-  [trips args]
-  (-> trips
+  [args]
+  (-> (trip/load-trips)
       (trip/add-trip args)
       (trip/save-trips)))
 
 (defn delete-trip
-  [trips args]
-  (-> trips
+  [args]
+  (-> (trip/load-trips)
       (trip/delete-trip args)
       (trip/save-trips)))
 
 (defn shift-trip
-  [trips [pattern days]]
-  (-> trips
+  [[pattern days]]
+  (-> (trip/load-trips)
       (trip/shift-trip [pattern (read-string days)])
       (trip/save-trips)))
 
 (defn list-trips
-  [trips args]
-  (->> trips
+  [args]
+  (->> (trip/load-trips)
        (map trip/format-trip)
-       (clojure.string/join "\n")))
+       (clojure.string/join "\n")
+       println))
 
 (def actions
   {:draw   draw-chart
-   :add    add-trip
-   :delete delete-trip
-   :shift  shift-trip
+   :add    (juxt add-trip draw-chart)
+   :delete (juxt delete-trip draw-chart)
+   :shift  (juxt shift-trip draw-chart)
    :list   list-trips})
 
 (defn -main [action & args]
-  (let [trips (trip/load-trips)]
-    (some-> action
-            keyword
-            actions
-            (apply [trips args])
-            println)))
+  (some-> action
+          keyword
+          actions
+          (apply [args])))
 
